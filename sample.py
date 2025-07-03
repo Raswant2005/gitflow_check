d39ac71f51f5b4ea4799962a7cafb7bc36569cb0
Raswant2005 <144604084+Raswant2005@users.noreply.github.com>
2025-07-03 10:49:47 +0530

    added a line in sample.py
    printed * for separation and added a line

--- sample.py
+++ sample.py
@@ -2,3 +2,5 @@ print("Heelo!! This is my first python file")
 var = "Raswant"
 age = 20
 print(f"Myself {var} and my age is {age}")
+print("**"*10)
+print("this line was added in remote repository")
